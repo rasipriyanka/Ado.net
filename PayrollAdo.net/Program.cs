da7bb48e11e9450a5e31aa7b42a7a0d313355819
rasipriyanka <priyankapendela111@gmail.com>
2022-02-20 20:00:08 +0530

    program file

---
+++ PayrollAdo.net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayrollAdo.net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Retrieve data from database!");
+        }
+    }
+}
